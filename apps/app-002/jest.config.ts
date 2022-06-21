b41da34d9a0b7f3731198b0e4fdcf3d0893e4728
Alper Çıtak <citak.alper@gmail.com>
2022-06-21 17:55:09 +0300

    added app: app-002

---
+++ apps/app-002/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'app-002',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/app-002',
+};
